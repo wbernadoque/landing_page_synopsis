0fe8a4d4d4d4fa7021a0c8f1263a86e7d89965cb
Wesley <wnbeq@hotmail.com>
2024-03-11 14:38:16 -0300

    synopsis html

--- src/components/DownloadApp/index.js
+++ src/components/DownloadApp/index.js
@@ -1,18 +1,18 @@
-import { Container } from './styles';
-import Button from '../Button';
-import { BrowserView, MobileView } from 'react-device-detect';
-import { QRCodeSVG } from 'qrcode.react';
-import { Link } from 'react-router-dom';
+import { Container } from "./styles";
+import Button from "../Button";
+import { BrowserView, MobileView } from "react-device-detect";
+import { QRCodeSVG } from "qrcode.react";
+import { Link } from "react-router-dom";
 
 export default function DownloadApp({ area, colS, colE, rowS, rowE }) {
   function getOperatingSystem(window) {
-    let operatingSystem = 'Not known';
-    if (window.navigator.appVersion.indexOf('Android') !== -1) {
-      operatingSystem = 'Google Play';
-    } else if (window.navigator.appVersion.indexOf('iPhone') !== -1) {
-      operatingSystem = 'App Store';
+    let operatingSystem = "Not known";
+    if (window.navigator.appVersion.indexOf("Android") !== -1) {
+      operatingSystem = "Google Play";
+    } else if (window.navigator.appVersion.indexOf("iPhone") !== -1) {
+      operatingSystem = "App Store";
     } else {
-      operatingSystem = 'loja de aplicativos';
+      operatingSystem = "loja de aplicativos";
     }
     return operatingSystem;
   }
@@ -24,18 +24,18 @@ export default function DownloadApp({ area, colS, colE, rowS, rowE }) {
         <div
           style={{
             borderRadius: 4,
-            border: '8px solid #fff',
-            width: 'auto',
-            height: 'auto',
+            border: "8px solid #fff",
+            width: "auto",
+            height: "auto",
           }}
         >
-          <QRCodeSVG value="https://synopsis-ia.com/redirection" />
+          <QRCodeSVG value="https://wbernadoque.github.io/landing_page_synopsis/redirection" />
         </div>
       </BrowserView>
       <MobileView>
         <Link
-          to="https://synopsis-ia.com/redirection"
-          style={{ textDecoration: 'none' }}
+          to="https://wbernadoque.github.io/landing_page_synopsis/redirection"
+          style={{ textDecoration: "none" }}
         >
           <Button icone="download" system={system} />
         </Link>
